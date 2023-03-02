5d357dc65ffbca1bc6007c7d01a15616d7103e8a
suheon.lim <suheon.lim@lemonhc.com>
2023-03-02 17:56:37 +0900

    thymeleaf setting clear

---
+++ src/main/java/com/board/single/controller/WebController.java
@@ -0,0 +1,16 @@
+package com.board.single.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class WebController{
+
+    // 메인
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
